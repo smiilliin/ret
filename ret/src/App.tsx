3a8b45b0000038cf956cc615b50f7664e71ce44b
smiilliin <smiilliindeveloper@gmail.com>
2023-11-18 15:36:20 +0900

    added eslint

--- ret/src/App.tsx
+++ ret/src/App.tsx
@@ -12,7 +12,12 @@ function App() {
       <header className="App-header">
         <img src={icon} className="App-logo" alt="logo" />
         <p>RET - React Electron Typescript</p>
-        <a className="App-link" href="https://github.com/smiilliin" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://github.com/smiilliin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           smiilliin
         </a>
       </header>
