345aaac35a16cc8e7ca94f04ab26c73f42224c5e
smiilliin <smiilliindeveloper@gmail.com>
2023-11-18 22:31:56 +0900

    fixed can't detect listening due to SO_REUSEADDR

--- dev.js
+++ dev.js
@@ -1,38 +1,16 @@
 const { spawn } = require("cross-spawn");
-const net = require("net");
+const tcpPortUsed = require("tcp-port-used");
 
 const isPortOpen = (port) => {
-  return new Promise((resolve) => {
-    // const socket = new net.();
-    const socket = new net.createServer();
-
-    // socket.on("timeout", () => {
-    //   socket.destroy();
-    //   resolve(false);
-    // });
-
-    socket.on("error", (error) => {
-      resolve(false);
-      // if (error.code !== "ECONNREFUSED") resolve(false);
-      // else resolve(true);
-    });
-    // socket.on("connect", () => {
-    //   socket.destroy();
-    //   resolve(false);
-    // });
-    socket.once("listening", function () {
-      socket.close();
-      resolve(true);
-    });
-    // socket.connect(port, "127.0.0.1");
-    // socket.setTimeout(500);
-    socket.listen(port);
-  });
+  return tcpPortUsed.check(port, "127.0.0.1");
 };
 
 (async () => {
   let port = 2999;
-  while (!(await isPortOpen(++port))) {}
+  while (await isPortOpen(++port)) {}
 
-  spawn("npm run dev:script", { env: { PORT: port, DEV: "true", BROWSER: "NONE" }, stdio: "inherit" });
+  spawn("npm run dev:script", {
+    env: { PORT: port, DEV: "true", BROWSER: "NONE" },
+    stdio: "inherit",
+  });
 })();
